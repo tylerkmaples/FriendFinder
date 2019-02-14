9d0e8ab358d6c0111ee8a9d84ca2a1d0ced8ddec
TKMCode <tyler.code.maples@gmail.com>
2019-02-13 21:04:37 -0800

    finished

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -12,12 +12,31 @@ module.exports = function(app) {
     // Handle incoming survey results and compatability logic.
     app.post("/api/friends", function(req, res) {
         var newFriend = req.body;
-        newFriend.routeName = newFriend.name.replace(/\s+/g, "").toLowerCase();
+        // newFriend.routeName = newFriend.name.replace(/\s+/g, "").toLowerCase();
+        var match;
+        var difference = 40;
     
         console.log(newFriend);
+
+        for (var i = 0; i < friends.length; i++){
+            var total = 0;
+            console.log("Checking friend: " + friends[i].name + " compatibility...");
+            var friendsScores = friends[i].scores;
+            console.log(friendsScores);
+            friendsScores.forEach(function(score, index){
+                total += Math.abs(parseInt(score) - parseInt(newFriend.scores[index]));
+            });
+            console.log(`Compatibility score: (${total})`);
+            if (total < difference){
+                difference = total;
+                match = friends[i]
+            }
+        }
+        console.log("Your closest friend is: " + match.name);
     
         friends.push(newFriend);
     
-        res.json(newFriend);
+        res.json(match);
+        console.log(friends);
     });
 }  
