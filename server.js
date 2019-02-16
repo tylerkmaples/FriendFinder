fe39489c7a9e3f8c1122cee70e1ea576fef04e75
TKMCode <tyler.code.maples@gmail.com>
2019-02-16 12:01:30 -0800

    public test

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var PORT = process.env.PORT || 3000;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 
 // router
 require('./app/routing/apiRoutes')(app);
