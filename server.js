89bedf27efcbd3283329a31c63d5684a6de85011
TKMCode <tyler.code.maples@gmail.com>
2019-02-17 19:56:19 -0800

    test

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // app.use(express.static("app/public"));
-app.use(express.static(path.join(__dirname, 'app/public')))
+app.use('/public', express.static(path.join(__dirname, 'app/public')))
 
 // router
 require('./app/routing/apiRoutes')(app);
